fdd9451415fd5de1f2295a59f2cda23aa5935150
Will Woods <wwoods@redhat.com>
2016-11-03 18:11:41 -0400

    parser: make Lead own the 'name' string
    ..use a String instead of a &str and now Lead correctly owns that data.

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
 use std::str::from_utf8;
+use std::string::String;
 use nom::{be_u8, be_u16, be_u32, be_i32, be_u64, IResult};
 
 // TODO: use tag::TagID;
@@ -7,12 +8,12 @@ pub type TagID = i32;
 // HERE'S OUR CORE DATA TYPES / STRUCTS / ENUMS, YAYYYYY
 
 #[derive(Debug,PartialEq,Eq)]
-pub struct Lead<'a> {
+pub struct Lead {
     pub major:          u8,         // file format major version number (0x03)
     pub minor:          u8,         // file format minor version number (0x00)
     pub rpm_type:       u16,        // package type (0x00 = binary, 0x01 = source)
     pub archnum:        u16,        // if binary: package arch (0x01 = i386, etc.)
-    pub name:           &'a str,    // actually a NUL-terminated [u8;66]
+    pub name:           String,     // actually a NUL-terminated [u8;66]
     pub osnum:          u16,        // if binary: package OS (0x01 = Linux)
     pub signature_type: u16,        // package signature type (0x05)
 }
@@ -84,7 +85,7 @@ macro_rules! take_cstr (
     );
 );
 
-named!(parse_lead(&[u8]) -> Lead,
+named!(pub parse_lead(&[u8]) -> Lead,
     chain!(
         tag!([0xED, 0xAB, 0xEE, 0xDB]) ~ // the tilde chains items together
         maj:  be_u8  ~
@@ -97,7 +98,7 @@ named!(parse_lead(&[u8]) -> Lead,
         take!(16), // the chain ends with a comma
         // closure yields our return value
         || { Lead {major: maj, minor: min, rpm_type: typ, archnum: arch,
-                   name: name, osnum: os, signature_type: sig} }
+                   name: name.to_string(), osnum: os, signature_type: sig} }
   )
 );
 
@@ -253,7 +254,7 @@ fn parse_lead_ok() {
             minor: 0,
             rpm_type: 0,
             archnum: 1,
-            name: "hardlink-1:1.0-23.fc24",
+            name: String::from("hardlink-1:1.0-23.fc24"),
             osnum: 1,
             signature_type: 5,
         }
