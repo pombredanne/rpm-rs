a6b30d42460922e9fd4a1509d26283c63328728e
Will Woods <wwoods@redhat.com>
2017-01-16 14:28:45 -0500

    Switch from LGPLv2.1 to LGPLv2.1+
    ...LGPLv2.1 isn't compatible with the Apache-2.0 license, which is used
    by a lot of Rust crates (although not this one, at the moment).
    
    LGPLv3 *is* Apache-2.0 compatible, so LGPLv2.1+ should be fine.

--- src/bin/rpmdump.rs
+++ src/bin/rpmdump.rs
@@ -3,15 +3,16 @@
  * Copyright (c) 2017, Red Hat, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU Lesser General Public License,
- * version 2.1, as published by the Free Software Foundation.
+ * under the terms and conditions of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
  * more details.
  *
- * Authors: 
+ * Authors:
  *   Will Woods <wwoods@redhat.com>
  */
 extern crate rustc_serialize;
--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,11 @@
 /* parser.rs - nom-based parser for reading RPM files
- * 
+ *
  * Copyright (c) 2017, Red Hat, Inc.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU Lesser General Public License,
- * version 2.1, as published by the Free Software Foundation.
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
